95c8934e3196b54fd6c2a9885f81a1ffe2ef586d
Sidhartha Mahapatra <mahapatrasidhartha152@gmail.com>
2026-09-08 00:22:28 +0530

    Fix token extraction from authorization header

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -14,7 +14,7 @@ export const protect = async (req, res, next) => {
       });
     }
 
-    const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[2];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
